aef6a01febada2461ae54815398b9d24cbd2adc4
cleverfox <devel@viruzzz.org>
2024-03-26 16:03:12 +0300

    and new tx constants

--- include/tx_const.hrl
+++ include/tx_const.hrl
@@ -1,16 +1,21 @@
 %This file was generated automatically by bin/generate_headers script
 % from priv/tx_const.json.
 % You will lose all custom changes in this file.
+decode_purpose(35) -> gashint;
+decode_purpose(33) -> srcfeehint;
 decode_purpose(3) -> gas;
 decode_purpose(2) -> dstfee;
 decode_purpose(1) -> srcfee;
 decode_purpose(0) -> transfer;
 decode_purpose(N) when is_integer(N) -> N.
+encode_purpose(gashint) -> 35;
+encode_purpose(srcfeehint) -> 33;
 encode_purpose(gas) -> 3;
 encode_purpose(dstfee) -> 2;
 encode_purpose(srcfee) -> 1;
 encode_purpose(transfer) -> 0;
 encode_purpose(N) when is_integer(N) -> N.
+decode_kind(24) -> {2,chkey};
 decode_kind(23) -> {2,notify};
 decode_kind(22) -> {2,lstore};
 decode_kind(21) -> {2,tstore};
@@ -20,6 +25,7 @@ decode_kind(18) -> {2,deploy};
 decode_kind(17) -> {2,register};
 decode_kind(16) -> {2,generic};
 decode_kind(Any) -> throw({unknown_kind_id,Any}).
+encode_kind(2,chkey) -> 24;
 encode_kind(2,notify) -> 23;
 encode_kind(2,lstore) -> 22;
 encode_kind(2,tstore) -> 21;
